3bd044e55b90dd2c410ea87d4b2626dff5fa94f5
LAPTOP-T58V7O9R\ASUS <shenlongdemon@gmail.com>
2022-03-08 11:09:40 +0700

    fix release script

--- clear.sh
+++ clear.sh
@@ -1,6 +1,7 @@
 
 #!/bin/bash
 OS="`uname`"
+rm -rf $TMPDIR/react-*
 if  [[ $OS == MIN* ]]
 then
   echo "Has no watchman"
@@ -12,3 +13,5 @@ cd android && ./gradlew clean && cd ..
 npm cache verify && npm cache clean --force && rm -rf **/build/* && rm -rf **/.idea/*
 rm -rf ./android/build
 rm -rf ./android/app/build
+rm -r ./android/app/src/release/res
+rm -r ./android/app/build/intermediates
